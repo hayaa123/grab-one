7b2ff67f1423556d4f946ba3258412bc3dd3c019
hayaa1234 <hayalawansah@gmail.com>
2022-05-25 13:06:10 +0300

    finished initial subnav

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import Content from './Content'
 export class HomePage extends Component {
   render() {
     return (
+      <>
       <Navbar/>
+      <Content/>
+      </>
+      
     )
   }
 }
